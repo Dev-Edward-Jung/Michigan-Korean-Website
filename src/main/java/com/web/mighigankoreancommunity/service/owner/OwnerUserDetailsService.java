8b3a5c6a9a3ae265e75c4e6168ee8c4b05ce15a6
Youchan Jung <youchanj66@gmail.com>
2025-06-18 13:01:58 -0400

    employee login fixed

--- src/main/java/com/web/mighigankoreancommunity/service/owner/OwnerUserDetailsService.java
+++ src/main/java/com/web/mighigankoreancommunity/service/owner/OwnerUserDetailsService.java
@@ -19,7 +19,7 @@ public class OwnerUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        System.out.println("호출됨!");
+        System.out.println("Owner 호출됨!");
         Owner owner = ownerRepository.findOwnerByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Owner not found."));
         return new CustomUserDetails(owner);
